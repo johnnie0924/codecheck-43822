79a68e7d8c545845c11c82ccff032840ae08ddac
akiyama_kazuhiko <akiyama_kazuhiko@192.168.11.5>
2018-06-20 19:36:42 +0900

    しょかい

--- src/main/java/codecheck/App.java
+++ src/main/java/codecheck/App.java
@@ -2,9 +2,133 @@ package codecheck;
 
 public class App {
 	public static void main(String[] args) {
-		for (int i = 0, l = args.length; i < l; i++) {
-			String output = String.format("argv[%s]: %s", i, args[i]);
-			System.out.println(output);
-		}
+//		for (int i = 0, l = args.length; i < l; i++) {
+//			String output = String.format("argv[%s]: %s", i, args[i]);
+//			System.out.println(output);
+//		}
+		system.out.printoutln(callGet("aaa"));
+	}
+
+    private static Proxy proxy = new Proxy(Proxy.Type.HTTP, new InetSocketAddress("xxxxxxxx.co.jp", 80xx));
+    private static String proxySwitch = "1";
+
+
+    public static String callGet(String strGetUrl){
+
+        HttpURLConnection con = null;
+        StringBuffer result = new StringBuffer();
+
+        try {
+
+            URL url = new URL(strGetUrl);
+
+            if(proxySwitch.equals("1")){
+                con = (HttpURLConnection) url.openConnection(proxy);
+            }else{
+                con = (HttpURLConnection) url.openConnection();
+            }
+
+            con.setRequestMethod("GET");
+            con.connect();
+
+            // HTTPレスポンスコード
+            final int status = con.getResponseCode();
+            if (status == HttpURLConnection.HTTP_OK) {
+                // 通信に成功した
+                // テキストを取得する
+                final InputStream in = con.getInputStream();
+                String encoding = con.getContentEncoding();
+                if(null == encoding){
+                    encoding = "UTF-8";
+                }
+                final InputStreamReader inReader = new InputStreamReader(in, encoding);
+                final BufferedReader bufReader = new BufferedReader(inReader);
+                String line = null;
+                // 1行ずつテキストを読み込む
+                while((line = bufReader.readLine()) != null) {
+                    result.append(line);
+                }
+                bufReader.close();
+                inReader.close();
+                in.close();
+            }else{
+                System.out.println(status);
+            }
+
+        }catch (Exception e1) {
+            e1.printStackTrace();
+        } finally {
+            if (con != null) {
+                // コネクションを切断
+                con.disconnect();
+            }
+        }
+        System.out.println("result=" + result.toString());
+
+        return result.toString();
+
+
+    }
+
+
+    public static String callPost(String strPostUrl, String strContentType, String formParam){
+
+        HttpURLConnection con = null;
+        StringBuffer result = new StringBuffer();
+
+        try {
+
+            URL url = new URL(strPostUrl);
+
+            if(proxySwitch.equals("1")){
+                con = (HttpURLConnection) url.openConnection(proxy);
+            }else{
+                con = (HttpURLConnection) url.openConnection();
+            }
+
+            con.setDoOutput(true);
+            con.setRequestMethod("POST");
+            con.setRequestProperty("Content-Type", strContentType);
+            OutputStreamWriter out = new OutputStreamWriter(con.getOutputStream());
+            out.write(formParam);
+            out.close();
+            con.connect();
+
+            // HTTPレスポンスコード
+            final int status = con.getResponseCode();
+            if (status == HttpURLConnection.HTTP_OK) {
+                // 通信に成功した
+                // テキストを取得する
+                final InputStream in = con.getInputStream();
+                String encoding = con.getContentEncoding();
+                if(null == encoding){
+                    encoding = "UTF-8";
+                }
+                final InputStreamReader inReader = new InputStreamReader(in, encoding);
+                final BufferedReader bufReader = new BufferedReader(inReader);
+                String line = null;
+                // 1行ずつテキストを読み込む
+                while((line = bufReader.readLine()) != null) {
+                    result.append(line);
+                }
+                bufReader.close();
+                inReader.close();
+                in.close();
+            }else{
+                System.out.println(status);
+            }
+
+        }catch (Exception e1) {
+            e1.printStackTrace();
+        } finally {
+            if (con != null) {
+                // コネクションを切断
+                con.disconnect();
+            }
+        }
+        System.out.println("result=" + result.toString());
+
+        return result.toString();
+
 	}
 }
